b313f71aa9dd87e03208980d4560d54ab6d1cf0d
Bigghead <shavenchew@yahoo.com>
2018-03-24 23:11:44 -0700

    stream requests from node with RX

--- server/routes/hackerNews.js
+++ server/routes/hackerNews.js
@@ -2,20 +2,47 @@ const express = require( 'express' );
 const router  = express.Router();
 const axios   = require( 'axios' );
 
+const { zip } = require( 'rxjs/Observable' );
+const { Observable } = require( 'rxjs' );
 
-router.get( '/search/:hnId', ( req, res ) => {
+
+const obsHttp = ( url ) => {
+    return Observable.create( obs => {
+            return axios.get( url )
+                        .then( result => {
+                            obs.next( result.data );
+                        } )
+                        .catch( err => console.log( err ) )
+    } );
+}
+
+
+const obs = Observable.create( obs => obs.next( 1 ) )
+
+
+const handleHttpChunk = ( res, array, num ) => {
+
+    if( array.length === 0 ) return res.end();
+    let subArr = array.splice( 0, 20 );
+
+    return Observable.zip.apply( null, subArr.map( kid => {
+        return obsHttp( `https://hacker-news.firebaseio.com/v0/item/${kid}.json?print=pretty`)
+    } ) )
+    .subscribe( data => {
+        res.write( JSON.stringify( data ) )
+        handleHttpChunk( res, array, num ++ );
+    } )
+}
+
+
+router.get( '/search/:hnId', async ( req, res ) => {
    
     const { hnId } = req.params;
     axios.get( `https://hacker-news.firebaseio.com/v0/item/${hnId}.json?print=pretty`)
          .then( result => {
-             const kids = result.data.kids.slice(0,20);
-             return Promise.all( kids.map( kid => {
-                 return axios.get( `https://hacker-news.firebaseio.com/v0/item/${kid}.json?print=pretty`);
-             } ) )
-         } )
-         .then( allComments => {
-             const filtered = allComments.filter( kid => kid['data'].text.toLowerCase().includes('angular') );
-             res.json( filtered[0].data)
+             const kids = result.data.kids;
+             res.setHeader( 'Content-Type', 'application/json' );
+             handleHttpChunk( res, kids, 0 )
          } )
          .catch( err => console.log(err) )
 } );
