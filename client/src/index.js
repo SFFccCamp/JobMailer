935b7eb742ac3739253ef49fa4956739422e4689
Bigghead <shavenchew@yahoo.com>
2018-03-26 12:45:27 -0700

    bringing in redux and materialize

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import userReducer  from './store/reducers/userReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const rootReducer = combineReducers( {
+    user: userReducer
+} );
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, logger)
+);
+
+
+ReactDOM.render(
+    <Provider store={ store } >
+        <BrowserRouter>
+            <App store={ store }/>
+        </BrowserRouter>
+    </Provider>, 
+    document.getElementById('root'));
 registerServiceWorker();
