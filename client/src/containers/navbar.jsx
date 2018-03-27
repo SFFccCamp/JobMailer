978a1b0a1ef5f5c14b928b206cb02acc9d67eb9a
Bigghead <shavenchew@yahoo.com>
2018-03-27 12:56:27 -0700

    adding navbar

---
+++ client/src/containers/navbar.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+
+    isLoggedIn = ( user ) => {
+
+        return ( 
+            <div>
+                <li><a href="sass.html">{ user.username }</a></li>
+                <li><a href="sass.html">Log Out</a></li>
+            </div>
+        )
+    }
+
+    render() {
+
+        const user = this.props.store.getState();
+        return (
+            <nav className="grey darken-4 white-text">
+                <div className="nav-wrapper nav-container ">
+                    <a href="#" className="nav-logo">Job Mailer</a>
+                    <ul id="nav-mobile" className="hide-on-med-and-down">
+                        { user.isLoggedIn ? this.isLoggedIn() : (
+                            <li><a href="sass.html">Log In</a></li>
+                        ) }
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+
+export default Navbar;
