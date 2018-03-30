227747d26366d8c57dee33a567742feaba374627
Bigghead <shavenchew@yahoo.com>
2018-03-30 14:12:01 -0700

    adding nodemailer to send templates

--- server/routes/email.js
+++ server/routes/email.js
@@ -1,8 +1,19 @@
 const mongoose = require( 'mongoose' );
 const Users    = require( '../models/users' );
 const router   = require( 'express' ).Router();
+const mailer   = require( 'nodemailer' );
+const smtpTransport = require( 'nodemailer-smtp-transport' );
 
 const { escapeChars } = require( '../helpers/sanitize' );
+
+
+const transporter = mailer.createTransport( smtpTransport( {
+    service: 'gmail',
+    auth: {
+      user: process.env.mailUser,
+      pass: process.env.mailPass
+  }
+} ) )
 
 
 router.post( '/', async ( req, res ) => {
@@ -18,6 +29,16 @@ router.post( '/', async ( req, res ) => {
             { new: true }
         );
         if( !user ) throw new Error( 'No User Found' );
+         transporter.sendMail( {
+            from: address1,
+            to: address2,
+            subject: title, 
+            text: content
+        }, function( err, response ) {
+            if( err ) console.log( err )
+            console.log( response)
+        } );
+
         res.status( 200 ).json( { status: 'OK' } );
 
     } catch ( error ) {
